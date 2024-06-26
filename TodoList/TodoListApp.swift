2ca9813b9c6e4cbb68a42b2b885011bd84d23e1f
Ploux <peter@humo.golf>
2024-06-25 21:57:20 -0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Peter Loux on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
